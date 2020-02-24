bd2b3f6e5adec1046737d1fa374a4a68ed35ec7a
HugoElcabezas <hugo.gomezllanos@cetys.edu.mx>
2020-02-23 21:18:26 -0800

    NotaPrueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare let $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  ngOnInit() {
+    $('.collapsible').collapsible();
+  }
 }
